9fbb177eca5f920604acb0c8244ec0f3245d9897
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-12-13 17:07:11 -0600

    updated req

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (res, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Orders were fetched.'
     });
 });
 
-router.post('/', (res, res, next) => {
+router.post('/', (req, res, next) => {
     res.status(201).json({
         message: 'Orders were created.'
     });
 });
 
-router.get('/:orderID', (res, res, next) => {
+router.get('/:orderID', (req, res, next) => {
     res.status(200).json({
         message: 'Order details',
         orderID: req.params.orderID
     });
 });
 
-router.delete('/:orderID', (res, res, next) => {
+router.delete('/:orderID', (req, res, next) => {
     res.status(200).json({
         message: 'Order deleted',
         orderID: req.params.orderID

fd56e831ce399ac4368e9779a85583f8b3806118
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-12-13 17:18:23 -0600

    error handling

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+//middleware
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
+
+//error handling
+app.use((req, res, next) => {
+    const error = new Error('Not found.');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;

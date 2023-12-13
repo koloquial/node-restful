8dd2de783df3f5323b8b5741e90ba997354cb6c1
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-12-13 17:01:39 -0600

    patch and delete

--- api/routes/products.js
+++ api/routes/products.js
@@ -22,13 +22,27 @@ router.get('/:productID', (req, res, next) => {
         res.status(200).json({
             message: 'You discovered special ID',
             id: id
-        })
+        });
     }else{
         res.status(200).json({
             message: 'You passed an ID.',
             id: id
-        })
+        });
     }
+});
+
+//handle PATCH single product with id
+router.patch('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product.'
+    });
+});
+
+//handle DELTE single product with id
+router.delete('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product.'
+    });
 });
 
 module.exports = router;

0a427c710b593542db93d516aac25dbc9aecf538
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-12-13 20:08:03 -0600

    uptodate

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 //handle incoming GET requests
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Hande GET requests to /products'
+        message: '/products GET'
     });
 });
 
 //handle incoming POST requests
 router.post('/', (req, res, next) => {
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+
+    product.save()
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.log(error))
+
     res.status(201).json({
-        message: 'Hande POST requests to /products'
+        message: '/products POST',
+        created: product
     });
 });
 
--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+mongoose.connect(process.env.ATLAS);
+
 //middleware
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//cors
+app.use((res, req, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+})
+
+//routes
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
 
@@ -18,6 +41,7 @@ app.use((req, res, next) => {
     next(error);
 });
 
+//all other errors
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
